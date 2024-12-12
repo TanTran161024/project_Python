73822b781edff17cf6b9d914d5814e9a7f24df48
Kim Thuy <22130282@st.hcmuaf.edu.vn>
2024-12-12 21:15:16 +0700

    update register

--- app/views.py
+++ app/views.py
@@ -4,3 +4,14 @@ from django.http import Http404
 # Create your views here.
 def home(request):
     return render(request,'about.html')
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'register.html', context)
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
