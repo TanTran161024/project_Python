78e0f68c92a812b002608fb7cd0c20a1781632e1
Trần Ngọc Tân <22130247@st.hcmuaf.edu.vn>
2024-12-13 15:30:27 +0700

    xoa tat ca

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email' , 'password1', 'password2']
