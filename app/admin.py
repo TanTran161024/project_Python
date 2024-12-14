a273317a64dfbe847dbb45efbb4f6d31dfc1310b
Trần Ngọc Tân <22130247@st.hcmuaf.edu.vn>
2024-12-14 14:38:52 +0700

    xoa code cu

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost, Booking, Contact, Room, UserProfile
-
-# Đăng ký các mô hình vào Django Admin
-admin.site.register(BlogPost)
-admin.site.register(Booking)
-admin.site.register(Contact)
-admin.site.register(Room)
-admin.site.register(UserProfile)
